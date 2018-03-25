a339f0c266bd3aa7d7732e5b61533d16b2252c3c
user <magi@magi-desktop>
2018-03-25 16:42:43 +0900

    #77i3 transplant vm from .zshrc to indemendent file in happy-scripts

---
+++ vm.sh
@@ -0,0 +1,134 @@
+#!/bin/zsh
+
+# start Virtual Machine
+vm() {
+
+  VMX=`find . | grep -E '\.vmx$'`
+
+
+  if [ -n "$VMX" ]; then
+
+    if [ "$(uname)" = 'Darwin' ]; then
+      HOST="fusion"
+    else
+      HOST="player"
+    fi
+    # http://www.japan-secure.com/entry/how_to_add_a_snapshot_function_in_vmware_workstation_player.html
+    ENABLED=`grep -E '^scsi0:0.mode = "independent-nonpersistent' $VMX`
+
+    while getopts d:DhHlrsSt:R OPT
+    do
+      case $OPT in
+        D)
+            vmrun -T $HOST stop $VMX hard
+            vmrun -T $HOST deleteVM $VMX
+            return 0
+            ;;
+        s)  vmrun -T $HOST list
+            vmrun -T $HOST listSnapshots $VMX true
+            echo toolsstate=`vmrun -T $HOST checkToolsState $VMX`
+            cat $VMX | grep independent
+            return 0
+            ;;
+        S)  sed -i".org" -e "/^scsi0:0.mode = \"independent-nonpersistent\"$/d" $VMX
+            if [ -n "$ENABLED" ]; then
+              echo "[Persistent] machine state is being preserved"
+            else
+              echo "[NONpersistent] machine state will be desposed of at shutdown"
+              sed -ie "/^scsi0:0\.fileName/a scsi0:0.mode = \"independent-nonpersistent\"" $VMX
+            fi
+            return 0
+          ;;
+        r) echo restart Virtual Machine..
+          vmrun -T $HOST reset $VMX hard
+          return 0
+          ;;
+        h) echo halt Virtual Machine..
+          vmrun -T $HOST stop $VMX soft
+          return 0
+          ;;
+        H) echo halt Virtual Machine..
+          vmrun -T $HOST stop $VMX hard
+          return 0
+          ;;
+        R) echo restore latest snapshot..
+          VMSD=`find . | grep -E '\.vmsd$'`
+          SNAPSHOT=`cat $VMSD | grep -E 'displayName = ".+"' | sed -e 's/snapshot.\.displayName = "//g' | sed -e 's/"//g' | tail -n 1`
+          echo snapshot=$SNAPSHOT
+          vmrun -T $HOST revertToSnapshot $VMX $SNAPSHOT
+          vmrun -T $HOST start $VMX
+          return 0
+          ;;
+        t) echo take snapshot $OPTARG ..
+          vmrun -T $HOST snapshot $VMX $OPTARG
+          return 0
+          ;;
+        d) echo delete snapshot $OPTARG ..
+          vmrun -T $HOST deleteSnapshot $VMX $OPTARG
+          return 0
+          ;;
+        l) vmrun -T $HOST listSnapshots $VMX
+          return 0
+          ;;
+      esac
+    done
+
+    echo starting $VMX by VMWare Player
+    vmrun -T $HOST start $VMX
+    return 0
+  fi
+
+  VBOX=`find . | grep -E '\.vbox$'`
+
+  if [ -n "$VBOX" ]; then
+    VBOXPATH=`pwd`/`echo $VBOX | sed -e 's/^\.\///'`
+
+    while getopts hHrRs OPT
+    do
+      case $OPT in
+        h) echo halt Virtual Machine..
+           VBoxManage controlvm $VBOXPATH acpipowerbutton
+           return 0
+           ;;
+        H) echo halt Virtual Machine..
+           VBoxManage controlvm $VBOXPATH poweroff
+           return 0
+           ;;
+        r) echo restart Virtual Machine..
+           VBoxManage controlvm $VBOXPATH reset
+           return 0
+           ;;
+        R) echo restore snapshot of Virtual Machine..
+           VBoxManage controlvm $VBOXPATH poweroff
+           VBoxManage snapshot $VBOXPATH restorecurrent
+           virtualbox startvm $VBOXPATH
+           return 0
+           ;;
+        # TODO - make drive immutable
+        # https://www.virtualbox.org/manual/ch08.html
+        # http://qiita.com/heignamerican/items/fe02f61853f0217e238b
+        # vboxmanage storageattach pde --storagectl SATA --port 0 --device 0 --type hdd --medium none
+        # vboxmanage storageattach pde --storagectl SATA --port 0 --device 0 --type hdd --medium pde.vdi --mtype immutable
+        s) VBoxManage showvminfo $VBOXPATH | grep State
+           return 0
+           ;;
+      esac
+    done
+    # https://www.virtualbox.org/manual/ch08.html#vboxmanage-guestcontrol
+    # example:
+    # VBoxManage -nologo  guestcontrol "`pwd`/win8.vbox" run --exe "c:\\windows\\system32\\ipconfig.exe" --username **** --password "***" -v --wait-stdout --wait-stderr
+    echo starting $VBOXPATH by VirtualBox
+    # double
+    if [ `uname` = 'Darwin' ]; then
+      virtualbox startvm "$VBOXPATH"
+    elif [ `uname` = 'Linux' ]; then
+      printf "linux\n"
+      virtualbox startvm $VBOXPATH
+    fi
+    return 0
+  fi
+
+}
+
+vm $*
+
