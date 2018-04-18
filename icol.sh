73988dfc16141327e5dec9b7a4d6873decdf0aa3
user <>
2018-04-18 17:14:23 +0900

    #icol it was wrong condition, update regexp instead

--- icol.sh
+++ icol.sh
@@ -227,9 +227,8 @@ compose() {
 
 # main
 main() {
-  local IDRE="^\w{4}$"
-  #if [[ $1 =~ $IDRE ]]; then
-  if [[ -n $1 && [$1] =~ [$IDRE] ]]; then
+  local IDRE="^[0-9a-z]{4}$"
+  if [[ $1 =~ $IDRE ]]; then
     compose $1
     return 0
   fi
