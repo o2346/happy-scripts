f6c54b034313f51f6cd52115ce153de09b2290d9
user <magi@magi-desktop>
2018-04-22 13:19:55 +0900

    #jdwi update moc script

--- dev.vm.sh
+++ dev.vm.sh
@@ -30,13 +30,36 @@ get_vmname() {
   echo tmp_`LANG=c < /dev/urandom tr -dc a-z0-9 | head -c${1:-6};echo`
 }
 
-newvm() {
-  [ -z "`get_hpv $3 2> /dev/null`" ] && echo "no hypervisor found" >&2 && return 1
-  local new_cmd="new_`get_hpv $3`"
-  local vmname=$1
-  local iso=$2
-  $new_cmd "$vmname" "$iso"
+get_arghpv() {
+  for opt in "$@"; do
+    if [ "`echo $opt | grep -e --hpv=*`" ]; then
+      echo $opt | sed -e 's/--hpv=//'
+    fi
+  done
+}
+get_argvname() {
+  for opt in "$@"; do
+    if [ "`echo $opt | grep -e --name=*`" ]; then
+      echo $opt | sed -e 's/--name=//'
+    fi
+  done
 }
 
+newvm() {
+  local arghpv=`get_arghpv $*`
+  [ -z "`get_hpv $arghpv 2> /dev/null`" ] && echo "no hypervisor found" >&2 && return 1
+  local vname=$(get_vmname `get_argvname $*`)
+  local new_cmd="new_`get_hpv $arghpv`"
+  $new_cmd "$vname" "$1"
+}
+
+while getopts n: OPT
+do
+  case $OPT in
+    n)  newvm $OPTARG $*
+      exit 0
+      ;;
+  esac
+done
 newvm $*
 
