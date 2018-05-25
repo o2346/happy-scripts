8caed5bb1662496f10069010026790a640a27646
user <>
2018-05-25 17:48:39 +0900

    put funcs outside

--- wm.sh
+++ wm.sh
@@ -8,56 +8,57 @@
 # Confirmed functional with GNU Make 4.x on macos Sierra & Linux Mint18.x
 # depends on node.js(wm.js in the same directory) or fswatch
 # modified from http://zgp.org/~dmarti/tips/automatically-run-make/#.WY6eoDeRVhE
-_wm() (
 
-  make_prereqs() {
-    # Make "make" figure out what files it's interested in.
-    echo "Makefile"
-    make -dnr $* | tr ' ' '\n' | \
-        grep ".*'.$" | grep -o '\w.*\b' | sed "s/'\.//"
-  }
+make_prereqs() {
+  # Make "make" figure out what files it's interested in.
+  echo "Makefile"
+  make -dnr $* | tr ' ' '\n' | \
+      grep ".*'.$" | grep -o '\w.*\b' | sed "s/'\.//"
+}
 
-  prereq_files_verbose() {
-    # prerequisites mentioned in a Makefile
-    # that are extant files
-    echo ' '
-    for f in `make_prereqs $* | sort -u`; do
-      if [ -f $f ]; then
-        echo -n "$f "
+prereq_files_verbose() {
+  # prerequisites mentioned in a Makefile
+  # that are extant files
+  echo ' '
+  for f in `make_prereqs $* | sort -u`; do
+    if [ -f $f ]; then
+      echo -n "$f "
 
-        # file names defined in the target source like #include "hoge.h" also should be a target
-        # If they ware actually exists.
-        # This may cause of slow
-        find . -type f -follow -print | sed -e 's/^\.\///g' | while read line; do
-          if cat $f | grep $line > /dev/null ; then
-            echo "$line "
-          fi
-        done
-      fi
-    done
-  }
-
-  # omit verbose
-  prereq_files() {
-    echo `prereq_files_verbose | tr ' ' '\n' | sort | uniq`
-  }
-
-  # show target files to watch
-  if echo $* | grep -e '--debug' > /dev/null; then
-    printf "Watching following Files..\n`prereq_files | tr ' ' '\n' | sort`\n" >&2
-  fi
-
-  # say something if make say like
-  isup() {
-    make -n | egrep -i '(Nothing to be done for)|(is up to date)'
-  }
-
-  # execute what to do if needed
-  makeif() {
-    if test -z "`isup`"; then
-      make $*
+      # file names defined in the target source like #include "hoge.h" also should be a target
+      # If they ware actually exists.
+      # This may cause of slow
+      find . -type f -follow -print | sed -e 's/^\.\///g' | while read line; do
+        if cat $f | grep $line > /dev/null ; then
+          echo "$line "
+        fi
+      done
     fi
-  }
+  done
+}
+
+# omit verbose
+prereq_files() {
+  echo `prereq_files_verbose | tr ' ' '\n' | sort | uniq`
+}
+
+# show target files to watch
+if echo $* | grep -e '--debug' > /dev/null; then
+  printf "Watching following Files..\n`prereq_files | tr ' ' '\n' | sort`\n" >&2
+fi
+
+# say something if make say like
+isup() {
+  make -n | egrep -i '(Nothing to be done for)|(is up to date)'
+}
+
+# execute what to do if needed
+makeif() {
+  if test -z "`isup`"; then
+    make $*
+  fi
+}
+
+_wm() (
 
   # the first time execution
   make $*
