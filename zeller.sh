c8c237fd105536df219bc06f8f9c0189ff049f51
user <>
2018-06-08 16:15:33 +0900

    date from stdin

--- zeller.sh
+++ zeller.sh
@@ -3,9 +3,11 @@
 # https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=5&cad=rja&uact=8&ved=0ahUKEwjmmcaH4YTaAhVBE5QKHaI8CHEQFghJMAQ&url=https%3A%2F%2Fqiita.com%2Fbsdhack%2Fitems%2F2884a232bf49dbd7988c&usg=AOvVaw1oE0S8odLmnRf1EPO7hjt3
 
 _zeller() {
-  test $# -ne 3 && exit 1
+  stdin=`cat - | sed 's|[/-]||g'`
 
-  y=$1; m=$2; d=$3
+  y=`echo $stdin | awk '{print substr($0,0,4)}'`
+  m=`echo $stdin | awk '{print substr($0,5,2)}' | sed 's/^0*//g'`
+  d=`echo $stdin | awk '{print substr($0,7,2)}' | sed 's/^0*//g'`
   test $m -lt 3 && y=$(($y - 1)) && m=$(($m + 12))
   echo $(($(($y + $y/4 - $y/100 + $y/400 + $(($((13 * $m + 8))/5)) + $d)) % 7))
 }
@@ -14,5 +16,5 @@ _zeller() {
 if echo "$-" | grep -q "i"; then
   :
 else
-  _zeller $*
+  _zeller $* -
 fi
