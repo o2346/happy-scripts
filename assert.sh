e96cc58dd58fc571a1b9636cf81693794187c59d
user <>
2018-06-25 16:07:07 +0900

    update var

--- assert.sh
+++ assert.sh
@@ -5,19 +5,19 @@ _diff=`mktemp`
 # assert arg1 arg2
 assert() {
   [ "$1" = "$2" ]
-  local _affirmative=$?
+  local _is_affirmative=$?
   #"\e[29;1m`basename ${WD}`\e[m  \e[37;4m${WD}\e[m\n"
   local _ok="\e[32;1m[_____ok]\e[m"
   local _failure="\e[31;1m[FAILURE]\e[m"
-  [ "$_affirmative" = 0 ] && printf $_ok || printf $_failure
-  if [ "$_affirmative" != 0 ]; then
+  [ "$_is_affirmative" = 0 ] && printf $_ok || printf $_failure
+  if [ "$_is_affirmative" != 0 ]; then
     local _1=`mktemp`
     local _2=`mktemp`
     echo $1 > $_1
     echo $2 > $_2
     diff $_1 $_2 > $_diff
   fi
-  return $_affirmative
+  return $_is_affirmative
 }
 
 . $1
@@ -26,9 +26,9 @@ exit_code=0
 
 while read cmd; do
   eval $cmd
-  affirmative=$?
+  is_affirmative=$?
   printf ' -- '$cmd'\n'
-  [ "$affirmative" != 0 ] && cat $_diff >&2 && ((exit_code++))
+  [ "$is_affirmative" != 0 ] && cat $_diff >&2 && ((exit_code++))
 done < <(set | grep 'on_' | awk '{print $1}')
 
 exit $exit_code
