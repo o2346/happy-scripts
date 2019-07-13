9f18a21ca84ed55ebc9ecbca852884e16702a18c
user <magi@magi-desktop>
2019-07-13 15:48:02 +0900

    bug: adjust if noint

--- sudden_death.js
+++ sudden_death.js
@@ -1,7 +1,4 @@
-const balloonLeft = "＞　";
-const balloonRight = "　＜";
-
-
+const breaks = /\r\n|\n|\r/;
 /**
  * getOstensibleLength
  *
@@ -21,16 +18,23 @@ function getOstensibleLength( str ) {
  * @returns {undefined}
  */
 function buildLines( str ) {
+  const balloonLeft = "＞　";
+  const balloonRight = "　＜";
+
   let maxLength = 0;
-  return str.split( /[\r\n]+/ )
+  return str.split( breaks )
     .map( ( l ) => {
       return String().concat( balloonLeft, l, balloonRight );
     } )
     .map( ( l, i, a ) => {
       maxLength = Math.max( ...a.map( ( _l ) => { return getOstensibleLength( _l ); } ) );
       let ans = '';
-      if( getOstensibleLength( l ) < maxLength ) {
-        const pad = '　'.repeat( Math.round( maxLength - getOstensibleLength( l ) ) );
+      const distance = maxLength - getOstensibleLength( l );
+      if( distance > 0 ) {
+        let pad = String().concat( '　'.repeat( Math.ceil( distance ) ) );
+        if( !Number.isInteger( distance ) ) {
+          pad = pad.replace( /\s$/, ' ' );
+        }
         ans = l.replace( new RegExp( balloonRight + '$' ), pad + balloonRight );
       } else {
         ans = l;
@@ -52,7 +56,7 @@ function getBalloonUpperLower( str ) {
   const presufffixUpper = "＿";
   const presufffixLower = "￣";
 
-  const maxLength = Math.max( ...str.split( /[\n\r]/ ).map( ( _l ) => { return getOstensibleLength( _l ); } ) );
+  const maxLength = Math.max( ...str.split( breaks ).map( ( _l ) => { return getOstensibleLength( _l ); } ) );
   const upper = String().concat(
     presufffixUpper,
     balloonUpper.repeat( maxLength - 2 ),
@@ -76,48 +80,12 @@ function suddenDeath( str ) {
   if( !str ) {
     return null;
   }
-  //balloon
-  const balloonUpper = "人";
-  const balloonLower = "^Y";
-  let top = "＿人人人";
-  let bottom = "\r\n￣Y^Y^Y";
-  let cnt = 0;
-  let line = str.match( /\r\n|\n/g );
-  if( !line ) {
-    line = "";
-  }
-  line = line.length + 1;
-  if( line > 1 ) {
-    str = str.split( /\r\n|\r|\n/ );
-    for( let i = 0; i < line; i++ ) {
-      if( cnt < str[ i ].length ) {
-        cnt = str[ i ].length;
-      }
-      str[ i ] = balloonLeft + str[ i ] + balloonRight;
-    }
-    str = str.join( "\r\n" );
-    str = str.replace( /\r\n$/, "" );
-  } else {
-    cnt = str.length;
-    str = balloonLeft + str + balloonRight;
-  }
-  //6文字以上でずれるっぽいので調整してもいいかも・全角半角でカウント方法を調整しないと調整不可
-  for( let i = 1; i < cnt; i++ ) {
-    top += balloonUpper;
-    bottom += balloonLower;
-  }
-  top += "＿\n";
-  if( cnt > 1 ) {
-    bottom = bottom.replace( /\^Y$/m, "" );
-  }
-  bottom += "￣";
-  str = top + str + bottom;
-  //console.log( top + '\n' );
-  return str;
+  return [
+    getBalloonUpperLower( buildLines( str ) )[ 0 ],
+    buildLines( str ),
+    getBalloonUpperLower( buildLines( str ) )[ 1 ]
+  ].join( '\n' );
 }
 
-const arg = '突然aaaaの死突然の死突然の死\nいiiiiいいいいい';
+const arg = '突然の死\n\nSudden Death\nぼくアルバイトぉｫｫｫｫ\n123あああ';
 console.log( suddenDeath( arg ) );
-console.log( getBalloonUpperLower( buildLines( arg ) )[ 0 ] );
-console.log( buildLines( arg ) );
-console.log( getBalloonUpperLower( buildLines( arg ) )[ 1 ] );
