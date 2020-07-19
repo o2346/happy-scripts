3c365ba7c3fae7a430e9584ea6e76147d6aa1c59
o2346 <magi@magi-desktop>
2020-07-19 12:24:28 +0900

    refactor

--- deploy.files.sh
+++ deploy.files.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
+#Usage:
+#curl 'https://raw.githubusercontent.com/o2346/happy-scripts/develop/deploy.files.sh' | bash -s - gist:xxxx
+#gist:xxxx is a example url
+
 # Deploy dotfiles in a manner indicated below
 #https://wiki.archlinux.org/index.php/Dotfiles
 #https://news.ycombinator.com/item?id=11070797
 
 readonly url=$1
-readonly local_name=Documents/.files
+readonly local_name=.files
 
 readonly workdir=`mktemp -d`
 
 cd $workdir
 
-git clone --separate-git-dir=$HOME/${local_name} $url ./.files || exit 1
-cd .files
-ls -al
-pwd
+git clone --separate-git-dir=$HOME/${local_name} $url $PWD/.files || exit 1
+cd $PWD/.files
 
-for f in .??*
-do
-    case $f in
-      .git) continue;;
-      .gitignore) continue;;
-      .DS_Store) continue;;
-      default) :
-    esac
-    [ -f ~/$f ] || continue
-    cp -i $(pwd)/$f ~/ 2>&1
-done
+echo "Publishing dotfiles into $HOME but will not overwrite existing ones" >&2
+ls $workdir/.files/.??* | grep -v git | xargs -I{} cp -vn {} ~/
 
 cd $HOME/$local_name
-#git config --bool core.bare true
 git config status.showUntrackedFiles no
 git push --set-upstream origin master
+git --git-dir=$HOME/$local_name --work-tree=$HOME pull
 
 echo 'alias config=/usr/bin/git\ --git-dir=$HOME/'$local_name'\ --work-tree=$HOME' >&2
+
+#https://stackoverflow.com/questions/8514284/bash-how-to-pass-arguments-to-a-script-that-is-read-via-standard-input
