bd614a2e4967c237f2ca9a439fc5296d5b51625e
o2346 <o2346@hotmail.com>
2020-07-22 23:33:37 +0900

    refactor

--- install.uc.sh
+++ install.uc.sh
@@ -6,35 +6,37 @@
 #https://news.ycombinator.com/item?id=11070797
 #https://www.atlassian.com/git/tutorials/dotfiles
 
+#before executing, ensure current directory is where you intent to deploy the files
+#for instance, $HOME for practical. `mktemp -d` for testing instead
+
+# if any objects were alredy there, it aborts
+
 #Usage:
 #curl 'https://raw.githubusercontent.com/o2346/happy-scripts/develop/install.uc.sh' | bash -s - [REMOTE_URL]
-#[REMOTE_URL] like gist:xxxx foe instance assumined already could be cloned as a bare
+#[REMOTE_URL] like gist:xxxx for instance assumined already could be cloned as a bare
+
 #demo:
 #curl 'https://raw.githubusercontent.com/o2346/happy-scripts/develop/install.uc.sh' | bash -s - https://gist.github.com/o2346/d12142dc810a6b5175607a19ed3c6373
 
 readonly url=$1
 readonly local_name='.uc'
 
-readonly workdir=`mktemp -d`
+git clone --bare $url $PWD/$local_name || exit 1
 
-cd $workdir
-
-git clone --separate-git-dir=$HOME/${local_name} $url $PWD/.uc || exit 1
-
-cd $HOME/$local_name
-#git --git-dir=$HOME/$local_name --work-tree=$HOME checkout .
+cd $local_name
 git config status.showUntrackedFiles no
-git push --set-upstream origin master
-#git --git-dir=$HOME/$local_name --work-tree=$HOME pull
 
-echo "Issue following commands" >&2
-echo 'alias .uc=/usr/bin/git\ --git-dir=$HOME/'$local_name'\ --work-tree=$HOME' >&2
-echo ".uc checkout ." >&2
+echo "Issue command like below to operate git dedicated for the repo" >&2
+echo "alias config=/usr/bin/git\ --git-dir=$PWD\ --work-tree=`dirname $PWD`" >&2
 
-#After successful '.uc' command would be available behaves as special git command
+if ! git --git-dir=$PWD --work-tree=`dirname $PWD` checkout; then
+  echo "Something is wrong. Carefully remove related objects and retry" >&2
+  exit 1
+fi
 
-#https://dev.classmethod.jp/articles/git-reset-and-git-checkout/
-#or
-#echo 'git --git-dir=$HOME/$local_name --work-tree=$HOME checkout .'
+echo 'looks fine review the files' >&2
+cd ../
+ls -altrh | tail
 
 #https://stackoverflow.com/questions/8514284/bash-how-to-pass-arguments-to-a-script-that-is-read-via-standard-input
+
