5f80ff702fb65eeac32d9624efb9d89042093d42
o2346 <o2346@hotmail.com>
2020-07-26 09:31:26 +0900

    indent 4 -> 2

--- extract.sh
+++ extract.sh
@@ -2,62 +2,62 @@
 #http://xgarrido.github.io/zsh-utilities/zsh-utilities-functions.html
 function _extract ()
 {
-    local success
-    local file_name
-    local extract_dir
+  local success
+  local file_name
+  local extract_dir
 
-    if [[ "$1" == "" ]]; then
-        echo "Usage: extract [-option] [file ...]"
-        echo
+  if [[ "$1" == "" ]]; then
+    echo "Usage: extract [-option] [file ...]"
+    echo
+  fi
+
+  while [ -n "$1" ]; do
+    if [[ ! -f "$1" ]]; then
+      pkgtools__msg_warning "'$1' is not a valid file"
+      shift
+      continue
     fi
 
-    while [ -n "$1" ]; do
-        if [[ ! -f "$1" ]]; then
-            pkgtools__msg_warning "'$1' is not a valid file"
-            shift
-            continue
-        fi
+    success=0
+    file_name="$( basename "$1" )"
+    extract_dir="$( echo "$file_name" | sed "s/\.${1##*.}//g" )"
+    case "$1" in
+      (*.tar.gz|*.tgz) tar xvzf "$1" ;;
+      (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
+      (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
+        && tar --xz -xvf "$1" \
+        || xzcat "$1" | tar xvf - ;;
+      (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
+        && tar --lzma -xvf "$1" \
+        || lzcat "$1" | tar xvf - ;;
+      (*.tar) tar xvf "$1" ;;
+      (*.gz) gunzip "$1" ;;
+      (*.bz2) bunzip2 "$1" ;;
+      (*.xz) unxz "$1" ;;
+      (*.lzma) unlzma "$1" ;;
+      (*.Z) uncompress "$1" ;;
+      (*.zip) unzip "$1" -d $extract_dir ;;
+      (*.rar) unrar e -ad "$1" ;;
+      (*.7z) 7za x "$1" ;;
+      (*.deb)
+        mkdir -p "$extract_dir/control"
+        mkdir -p "$extract_dir/data"
+        cd "$extract_dir"; ar vx "../${1}" > /dev/null
+        cd control; tar xzvf ../control.tar.gz
+        cd ../data; tar xzvf ../data.tar.gz
+        cd ..; rm *.tar.gz debian-binary
+        cd ..
+        ;;
+      (*)
+        pkgtools__msg_error "'$1' cannot be extracted" 1>&2
+        success=1
+        ;;
+    esac
 
-        success=0
-        file_name="$( basename "$1" )"
-        extract_dir="$( echo "$file_name" | sed "s/\.${1##*.}//g" )"
-        case "$1" in
-            (*.tar.gz|*.tgz) tar xvzf "$1" ;;
-            (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
-            (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
-                && tar --xz -xvf "$1" \
-                || xzcat "$1" | tar xvf - ;;
-            (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
-                && tar --lzma -xvf "$1" \
-                || lzcat "$1" | tar xvf - ;;
-            (*.tar) tar xvf "$1" ;;
-            (*.gz) gunzip "$1" ;;
-            (*.bz2) bunzip2 "$1" ;;
-            (*.xz) unxz "$1" ;;
-            (*.lzma) unlzma "$1" ;;
-            (*.Z) uncompress "$1" ;;
-            (*.zip) unzip "$1" -d $extract_dir ;;
-            (*.rar) unrar e -ad "$1" ;;
-            (*.7z) 7za x "$1" ;;
-            (*.deb)
-                mkdir -p "$extract_dir/control"
-                mkdir -p "$extract_dir/data"
-                cd "$extract_dir"; ar vx "../${1}" > /dev/null
-                cd control; tar xzvf ../control.tar.gz
-                cd ../data; tar xzvf ../data.tar.gz
-                cd ..; rm *.tar.gz debian-binary
-                cd ..
-                ;;
-            (*)
-                pkgtools__msg_error "'$1' cannot be extracted" 1>&2
-                success=1
-                ;;
-        esac
-
-        #(( success = $success > 0 ? $success : $? ))
-        #(( $success == 0 )) && rm "$1"
-        shift
-    done
-    return 0
+    #(( success = $success > 0 ? $success : $? ))
+    #(( $success == 0 )) && rm "$1"
+    shift
+  done
+  return 0
 }
 _extract $*
