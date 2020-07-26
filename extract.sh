a5100cf54f149ea4781c77ac104f5a1747dc7c54
o2346 <o2346@hotmail.com>
2020-07-26 11:14:33 +0900

    while loop unnecessary

--- extract.sh
+++ extract.sh
@@ -17,42 +17,38 @@ function _extract () {
   local readonly file_name="$( basename "$1" )"
   local readonly extract_dir="$( echo "$file_name" | sed "s/\.${1##*.}//g" )"_extracted
 
-  while [ -n "$1" -a -f "$1" ]; do
-    case "$1" in
-      (*.tar.gz|*.tgz) tar xvzf "$1" ;;
-      (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
-      (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
-        && tar --xz -xvf "$1" \
-        || xzcat "$1" | tar xvf - ;;
-      (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
-        && tar --lzma -xvf "$1" \
-        || lzcat "$1" | tar xvf - ;;
-      (*.tar) tar xvf "$1" ;;
-      (*.gz) gunzip "$1" ;;
-      (*.bz2) bunzip2 "$1" ;;
-      (*.xz) unxz "$1" ;;
-      (*.lzma) unlzma "$1" ;;
-      (*.Z) uncompress "$1" ;;
-      (*.zip) unzip "$1" -d $extract_dir ;;
-      (*.rar) unrar e -ad "$1" ;;
-      (*.7z) 7za x "$1" ;;
-      (*.deb)
-        mkdir -p "$extract_dir/control"
-        mkdir -p "$extract_dir/data"
-        cd "$extract_dir"; ar vx "../${1}" > /dev/null
-        cd control; tar xzvf ../control.tar.gz
-        cd ../data; tar xzvf ../data.tar.gz
-        cd ..; rm *.tar.gz debian-binary
-        cd ..
-        ;;
-      (*)
-        echo "Error: '$1' cannot be extracted" 1>&2
-        return 3
-        ;;
-    esac
-
-    shift
-  done
+  case "$1" in
+    (*.tar.gz|*.tgz) tar xvzf "$1" ;;
+    (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
+    (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
+      && tar --xz -xvf "$1" \
+      || xzcat "$1" | tar xvf - ;;
+    (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
+      && tar --lzma -xvf "$1" \
+      || lzcat "$1" | tar xvf - ;;
+    (*.tar) tar xvf "$1" ;;
+    (*.gz) gunzip "$1" ;;
+    (*.bz2) bunzip2 "$1" ;;
+    (*.xz) unxz "$1" ;;
+    (*.lzma) unlzma "$1" ;;
+    (*.Z) uncompress "$1" ;;
+    (*.zip) unzip "$1" -d $extract_dir ;;
+    (*.rar) unrar e -ad "$1" ;;
+    (*.7z) 7za x "$1" ;;
+    (*.deb)
+      mkdir -p "$extract_dir/control"
+      mkdir -p "$extract_dir/data"
+      cd "$extract_dir"; ar vx "../${1}" > /dev/null
+      cd control; tar xzvf ../control.tar.gz
+      cd ../data; tar xzvf ../data.tar.gz
+      cd ..; rm *.tar.gz debian-binary
+      cd ..
+      ;;
+    (*)
+      echo "Error: '$1' cannot be extracted" 1>&2
+      return 3
+      ;;
+  esac
 
   return 0
 }
