ce6969c3bb5e53b54b380dac89c5e809f447a716
o2346 <o2346@hotmail.com>
2020-07-28 23:07:29 +0900

    update, line spacing can not be reduced..

--- urxvt.sh
+++ urxvt.sh
@@ -3,6 +3,9 @@
 
 readonly opacity=80
 
+#https://unix.stackexchange.com/questions/14159/how-do-i-find-the-window-dimensions-and-position-accurately-including-decoration
+#xwininfo -id $(xdotool getactivewindow)
+#xdotool getwindowfocus getwindowgeometry
 opts=(
 
   ## common settings
@@ -10,18 +13,19 @@ opts=(
   #-sr                          # scrollBar_right
   #-st                          # scrollBar_floating
   #-scrollstyle plain           # scrollstyle
-  -sb                          # scrollBar - on(default):-sb  off:+sb
+  +sb                          # scrollBar - on(default):-sb  off:+sb
   #-bc                          # cursorBlink
   #-uc                          # cursorUnderline
   -pointerBlank                # pointerBlank
   #-vb                          # visualBell
   -sl 8000                     # saveLines
   -fade 40                     # fading
+  #-bl # it brakes X ? killall urxvtd fixes the issue
 
   ## font
-  -fn 'xft:DejaVu Sans Mono-14, xft:IPAGothic'
+  -fn 'xft:VL Gothic-14, xft:IPAGothic'
   #-letsp -1                    # letterSpace
-  #-lsp 0                       # lineSpace
+  #-lsp -20                       # lineSpace #http://emonkak.hatenablog.com/entry/2016/12/09/185009
 
   ## color / opacity
   ## gruvbox-dark https://github.com/morhetz/gruvbox
@@ -58,3 +62,6 @@ urxvtc "${opts[@]}" >/dev/null 2>&1 || {
 }
 
 # http://malkalech.com/urxvt_terminal_emulator
+# debugging:
+# gitls | wr 'tsr l "C-l" "~/Documents/happy-scripts/urxvt.sh"'
+# C-d to exit shortcut
