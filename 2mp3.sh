e690dc97715ad7e953cb5d5f155d563da2968794
o2 <o2346@hotmail.com>
2023-01-09 12:31:57 +0900

    add dummy .mp3 to initiate randomize on specific app

--- 2mp3.sh
+++ 2mp3.sh
@@ -9,10 +9,15 @@ resolve() {
   #echo $wkdir/$underlined.mp4
 }
 
+# use "generate_dummy_mp 3 6" instead of below
+#dummy_basename=000_dummy_vlc
+#ffmpeg -f lavfi -i anullsrc=r=44100:cl=mono -t 6 -q:a 9 -acodec libmp3lame $wkdir/${dummy_basename}.mp3
+
 for f in **/*.{mp4,wav}; do
   [ -f "$f" ] || continue
   [ "`dirname \"$f\"`" = 'origin' ] && continue
   dirname "$f" | grep -i 'bored' > /dev/null && continue
   ffmpeg -i "$f" $* "`resolve ${f%.*}`"
 done
+
 echo $wkdir
