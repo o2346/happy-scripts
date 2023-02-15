9e4eba69ca0d3a4afaf0e76082a03c97606c1c1a
user <>
2023-02-15 21:54:30 +0900

    add help option, comments, message

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -2,19 +2,20 @@
 
 #https://discussions.apple.com/thread/250340095?answerId=250644020022#250644020022
 
-#usage example
-#% 4502 4503 80 user@externalhost
-#
-#in other terminal
-#curl localhost:4502
-#or go ther by any modern browser
-#
-#Press Ctrl+c to finish
-#
-#Assuming some server is running on externalhost:4502 as wellas port 4503 ahd 80.
-#ssh is capable to accsept multiple -L options.
-#
-#
+echo $@ | grep -E '(\-h|\-\-help)' > /dev/null && echo "
+Thanks to
+https://discussions.apple.com/thread/250340095?answerId=250644020022#250644020022
+
+usage example:
+`basename $0` 4502 4503 80 user@externalhost
+
+in other terminal,
+curl localhost:4502
+Or go there by any modern browser
+
+Press Ctrl+c to finish
+Assuming some server is running on externalhost:4502 as wellas port 4503 and 80.
+" && exit 0
 
 readonly userhost=${@: -1}
 
@@ -26,5 +27,7 @@ tunnel() {
 trap "ps aux | grep -E 'ssh -N -n -L.+localhost' | awk '{print \$2}' | xargs kill" ERR EXIT SIGKILL
 
 lopts="$(echo ${*%${!#}} | awk  'BEGIN{RS=" ";ORS=" "}; {print "-L "$1":localhost:"$1}')"
+echo "Digging ssh tunnel toward $userhost" >&2
+#ssh is capable to accsept multiple -L options.
 ssh -N -n $lopts $userhost
 
