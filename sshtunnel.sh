48cf69361816105d18b5feaef9eeccda76391022
user <>
2023-02-15 21:28:20 +0900

    not only for macos, remove unnecessary comments

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -2,20 +2,14 @@
 
 #https://discussions.apple.com/thread/250340095?answerId=250644020022#250644020022
 
-uname | grep -i 'Darwin' || exit 1
-
 readonly userhost=${@: -1}
 
 tunnel() {
   ssh -N -n ${1} ${2}
-  #${1}:localhost:${1}
 }
 
 trap "ps aux | grep -E 'ssh -N -n -L.+localhost' | awk '{print \$2}' | xargs kill" ERR EXIT SIGKILL
 
-#echo ${*%${!#}} | tr ' ' '\n' | while read p; do
-#  tunnel ${p} $userhost
-#done
 lopts="$(echo ${*%${!#}} | awk  'BEGIN{RS=" ";ORS=" "}; {print "-L "$1":localhost:"$1}')"
 ssh -N -n $lopts $userhost
 
