03276d1ce655f90f90cc53272e7945e87342bdab
Kazunari Sato <kazunaris@adobe.com>
2023-07-21 16:30:50 +0900

    add whatever it is

---
+++ sshtunnel2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#https://discussions.apple.com/thread/250340095?answerId=250644020022#250644020022
+
+uname | grep -i 'Darwin' || exit 1
+
+readonly userhost=${@: -1}
+
+tunnel() {
+  ssh -N -n ${1} ${2}
+  #${1}:localhost:${1}
+}
+
+trap "ps aux | grep -E 'ssh -N -n -L.+localhost' | awk '{print \$2}' | xargs kill" ERR EXIT SIGKILL
+
+#echo ${*%${!#}} | tr ' ' '\n' | while read p; do
+#  tunnel ${p} $userhost
+#done
+lopts="$(echo ${*%${!#}} | awk  'BEGIN{RS=" ";ORS=" "}; {print "-L "$1":localhost:"$1}' | sed -e 's/80:localhost:80/8080:localhost:80/g')"
+ssh -N -n $lopts $userhost
+
