c3487069b8b28b2888172c0794f33dcd4b0b2aaf
o2 <o2346@hotmail.com>
2023-12-29 12:30:43 +0900

    allow firewall

--- vm.sh
+++ vm.sh
@@ -763,11 +763,13 @@ _vm() {
       esac
     done
 
+    readonly default_allowed_ports='80\n443\n18383'
     readonly random_ssh_port=`get_random_ssh_port 2>/dev/null`
     #https://serverfault.com/a/704300
     readonly kvm_net_hostfwd_ssh="user,hostfwd=tcp::$random_ssh_port-:22"
     sudo firewall-cmd --zone=public --add-port=$random_ssh_port/tcp
-    readonly kvm_net_hostfwd_default="`printf '80\n443\n18383' | hostfwdtrans`"
+    readonly kvm_net_hostfwd_default="`printf "$default_allowed_ports" | hostfwdtrans`"
+    printf "$default_allowed_ports" | xargs -I{} sudo firewall-cmd --zone=public --add-port={}/tcp
     if [ -f "./hostfwd" ]; then
       kvm_net_hostfwd_miscs="`cat hostfwd | hostfwdtrans`"
       cat hostfwd | while read port; do
